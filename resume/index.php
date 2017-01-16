3738d2fe7292dbd2789b9f29169eafd8d74a8fe9
= <=>
2017-01-16 00:46:25 -0500

    added resume

---
+++ resume/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Kishons Portfolio</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Resume for Portfolio">
+    <meta name="keywords" content="Resume,Portfolio,JavaScript,Html,CSS">
+    <meta name="author" content="Kishon Diaz">
+    <script type="text/jscript" src="./scripts/jquery-3.1.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./css/maincss.css">
+  </head>
+  <body>
+    <header>
+      <div>
+        <img>
+        <span>Digisouls</span>
+      </div>
+    </header>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Gallery</a>
+        </li>
+        <li>
+          <a href="#">Codes</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">About Me</a>
+        </li>
+        <li>
+          <a href="#">Contact Me</a>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <div id="closed"></di>
+      <div id="open"></div>
+      <!--<div>
+        <img src="./assets/kishon_image.png" alt="Picture of Kishon" height="369" width="416">
+        <p>About Me</p>
+        <p>&nbsp;Currently I am student at Broward College obtaining my Associates degree in Computer Programming.<br>
+        I will be attending Broward College for my Bachelor’s degree for Information Technology, and this is my portfolio.</p>
+      </div>
+      <a>Click here to read more about me</a>-->
+    </main>
+    <script src="./scripts/mainscript.js"></script>
+  </body>
+</html>
